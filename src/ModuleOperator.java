8ef5eec2d8a1d75e4784b2660fbe7d8e46380c2b
Samu026 <149374089+Samu026@users.noreply.github.com>
2025-03-19 12:54:13 +0530

    Add files via upload

---
+++ src/ModuleOperator.java
@@ -0,0 +1,15 @@
+/* Write a program to find the Remainder when one number 
+   is divided by another using the modulus (%) Operator */
+
+public class ModuleOperator 
+{
+	public static void main (String args[])
+	{
+		int a =10;
+		int b = 3;
+		
+		int Remainder = a%b;
+		
+		System.out.println("Remainder:"+Remainder);
+	}
+}
