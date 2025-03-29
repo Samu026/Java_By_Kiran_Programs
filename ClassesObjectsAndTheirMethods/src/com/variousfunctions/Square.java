36c5f141ca86b8cd804704be79dab38887c467b9
Samu026 <149374089+Samu026@users.noreply.github.com>
2025-03-29 22:41:58 +0530

    Delete ClassesObjectsAndTheirMethods/src/com directory

--- ClassesObjectsAndTheirMethods/src/com/variousfunctions/Square.java
+++
@@ -1,8 +0,0 @@
-package com.variousfunctions;
-
-public class Square {
-	public void displaySquare(int num) {
-		int result = num * num;
-		System.out.println("Square of " + num + ": " + result);
-	}
-}
